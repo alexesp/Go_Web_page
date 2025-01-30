f89c2387577fe846b91b9ddcdf117d9db06c5d5b
alexesp <alexizaes@hotmail.com>
2025-01-30 14:28:40 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/alexesp/Go_Web_page.git
+
+go 1.23.5
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Test")
+	})
+
+	log.Fatal(http.ListenAndServe("localhost:8081", nil))
+}
